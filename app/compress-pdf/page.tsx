8c29514b1e3a3b69cd529bd76ceae67892320b3f
Ram <nishantramb1@gmail.com>
2026-05-21 22:12:29 +0530

    fixed pdf routes

---
+++ app/compress-pdf/page.tsx
@@ -0,0 +1,7 @@
+export default function CompressPDFPage() {
+    return (
+      <div className="min-h-screen bg-black text-white flex items-center justify-center text-5xl font-bold">
+        Compress PDF Tool
+      </div>
+    );
+  }
