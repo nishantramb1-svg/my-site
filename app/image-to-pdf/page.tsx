d8ecd54310fd4c2a23b325347912f4fd2c114812
Ram <nishantramb1@gmail.com>
2026-05-21 21:53:01 +0530

    updated website ui

---
+++ app/image-to-pdf/page.tsx
@@ -0,0 +1,7 @@
+export default function ImageToPDF() {
+    return (
+      <div className="min-h-screen bg-black text-white flex items-center justify-center text-5xl font-bold">
+        Image to PDF Tool
+      </div>
+    );
+  }
